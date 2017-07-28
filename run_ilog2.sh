a5c3b432b4bee555f67ab0bd9af09064c5e8643b
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2017-07-28 11:21:21 +0900

    Fix reproducibility bug

--- run_ilog2.sh
+++ run_ilog2.sh
@@ -8,6 +8,6 @@ ocamlbuild -use-ocamlfind -I extraction 'ilog2_print.native'
 cd ..
 
 cd c++
-g++ -std=c++11 -g -I/Users/jeehoon/Works/iRRAM/installed/include -Xlinker -rpath -Xlinker /Users/jeehoon/Works/iRRAM/installed/lib  ilog2-main.cc ilog2.cc  -L/Users/jeehoon/Works/iRRAM/installed/lib -liRRAM -lmpfr -lgmp -lm -lpthread -o ilog2
+g++ -std=c++11 -g -I/home/jeehoon.kang/Works/iRRAM/installed/include -Xlinker -rpath -Xlinker /home/jeehoon.kang/Works/iRRAM/installed/lib  ilog2-main.cc ilog2.cc  -L/home/jeehoon.kang/Works/iRRAM/installed/lib -liRRAM -lmpfr -lgmp -lm -lpthread -o ilog2
 ./ilog2
 cd ..
